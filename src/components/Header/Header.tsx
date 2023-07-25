2951e61b894870e81e22cffed19beb1be1918f7f
VasilenkoYurii <yura.vasilenko98@gmail.com>
2023-07-25 19:14:22 +0300

    added check mobile fun to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { ethers } from "ethers";
 import { JsonRpcSigner, Web3Provider } from "@ethersproject/providers";
+import { toast } from "react-hot-toast";
 import { setAddress, setBalance } from "../../redux/walletSlice";
 import { selectAddress, selectBalance } from "../../redux/selectors";
 import { formatAddress } from "../../helper/formatAddress";
@@ -32,16 +33,33 @@ export const Header = ({ signer, setSigner }: Props) => {
 
   const connectWallet = async () => {
     try {
-      const provider = new Web3Provider(window.ethereum);
-      // console.log("Ethereum provider detected!");
-      const { chainId } = await provider.getNetwork();
-      if (chainId !== 5) {
-        throw new Error("Change network to Goerli");
-      }
-      await provider.send("eth_requestAccounts", []);
-      const signer = provider.getSigner();
+      if (window.ethereum && window.ethereum.isMetaMask) {
+        const provider = new Web3Provider(window.ethereum);
+        // console.log("Ethereum provider detected!");
+        const { chainId } = await provider.getNetwork();
+        if (chainId !== 5) {
+          throw new Error("Change network to Goerli");
+        }
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
 
-      setSigner(signer);
+        setSigner(signer);
+        toast.success("You are connected!");
+      } else if (
+        window.innerWidth >= 200 &&
+        window.innerWidth <= 500 &&
+        !window.ethereum
+      ) {
+        toast.error(
+          "MetaMask app not found. Please download the app on your mobile device and use MetaMask mobile browser"
+        );
+        return;
+      } else {
+        toast.error(
+          "MetaMask extension not found. Please install the MetaMask extension on your browser"
+        );
+        return;
+      }
     } catch (err) {
       console.error("Error connecting wallet:", err);
     }
